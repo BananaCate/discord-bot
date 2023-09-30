289d91950a4ca5d4d32bcd82b77cf240e9cc0a33
BananaCate <alex.ragozin4@gmail.com>
2023-09-30 18:52:35 +0200

    made a startup message

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,16 @@
+const { WebhookClient } = require('discord.js');
+const { extraWebhookurl } = require('../../../config.json');
+const extraWebhook = new WebhookClient({ url: extraWebhookurl });
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(`Logged in as ${client.user.tag}.`);
+		
+		extraWebhook.send({
+			username: "Startup",
+			content: 'I have started up.'
+		});
 	},
 };
